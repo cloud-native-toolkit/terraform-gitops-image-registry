5babee7a380a822ceabbb753e8c24a60a5266d1b
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-06-13 16:35:05 +0530

    updated image-registry
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ locals {
   yaml_dir  = "${path.cwd}/.tmp/${local.name}/yaml"
   layer     = "infrastructure"
   application_branch = "main"
+  type  = "base"
   layer_config = var.gitops_config[local.layer]
   registry_type = replace(length(regexall(".*icr.io$", var.registry_server)) > 0 ? "icr_io" : var.registry_server, ".", "_")
   image_urls = {
@@ -55,7 +56,7 @@ resource null_resource create_yaml {
   }
 }
 
-resource null_resource setup_gitops {
+/*resource null_resource setup_gitops {
   depends_on = [null_resource.create_yaml]
 
   provisioner "local-exec" {
@@ -66,4 +67,40 @@ resource null_resource setup_gitops {
       GITOPS_CONFIG   = yamlencode(var.gitops_config)
     }
   }
+}*/
+
+resource null_resource setup_gitops {
+  depends_on = [null_resource.create_yaml]
+
+  triggers = {
+    name = local.name
+    namespace = var.namespace
+    yaml_dir = local.yaml_dir
+    server_name = var.server_name
+    layer = local.layer
+    type = local.type
+    git_credentials = yamlencode(var.git_credentials)
+    gitops_config   = yamlencode(var.gitops_config)
+    bin_dir = local.bin_dir
+  }
+
+  provisioner "local-exec" {
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+
+    environment = {
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
+    }
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+
+    environment = {
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
+    }
+  }
 }
+
--- test/stages/variables_old.tf
+++ test/stages/variables_old.tf
@@ -1,21 +1,18 @@
 
-variable cluster_username { 
+# Resource Group Variables
+variable "resource_group_name" {
   type        = string
-  description = "The username for AWS access"
+  description = "Existing resource group where the IKS cluster will be provisioned."
 }
 
-variable "cluster_password" {
+variable "ibmcloud_api_key" {
   type        = string
-  description = "The password for AWS access"
+  description = "The api key for IBM Cloud access"
 }
 
-variable "server_url" {
+variable "region" {
   type        = string
-}
-
-variable "bootstrap_prefix" {
-  type = string
-  default = ""
+  description = "Region for VLANs defined in private_vlan_number and public_vlan_number."
 }
 
 variable "namespace" {
@@ -40,6 +37,18 @@ variable "cluster_exists" {
   default     = "true"
 }
 
+variable "name_prefix" {
+  type        = string
+  description = "Prefix name that should be used for the cluster and services. If not provided then resource_group_name will be used"
+  default     = ""
+}
+
+variable "vpc_cluster" {
+  type        = bool
+  description = "Flag indicating that this is a vpc cluster"
+  default     = false
+}
+
 variable "git_token" {
   type        = string
   description = "Git token"
@@ -55,7 +64,7 @@ variable "git_type" {
 }
 
 variable "git_org" {
-  default = "cloud-native-toolkit-test"
+  default = "seansund"
 }
 
 variable "git_repo" {
@@ -67,11 +76,4 @@ variable "gitops_namespace" {
 }
 
 variable "git_username" {
-}
-
-variable "kubeseal_namespace" {
-  default = "sealed-secrets"
-}
-
-variable "cp_entitlement_key" {
 }
