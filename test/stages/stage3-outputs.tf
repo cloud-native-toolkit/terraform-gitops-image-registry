5d13bce0a91adf9b7ce7b4dbb148863c2862eff4
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-06-13 16:57:15 +0530

    updated stage
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -5,13 +5,13 @@ resource null_resource write_outputs {
 
     environment = {
       OUTPUT = jsonencode({
-        name        = module.gitops_module.name
-        branch      = module.gitops_module.branch
-        namespace   = module.gitops_module.namespace
-        server_name = module.gitops_module.server_name
-        layer       = module.gitops_module.layer
-        layer_dir   = module.gitops_module.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_module.layer == "services" ? "2-services" : "3-applications")
-        type        = module.gitops_module.type
+        name        = module.gitops_image_registry.name
+        branch      = module.gitops_image_registry.branch
+        namespace   = module.gitops_image_registry.namespace
+        server_name = module.gitops_image_registry.server_name
+        layer       = module.gitops_image_registry.layer
+        layer_dir   = module.gitops_image_registry.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_image_registry.layer == "services" ? "2-services" : "3-applications")
+        type        = module.gitops_image_registry.type
       })
     }
   }
