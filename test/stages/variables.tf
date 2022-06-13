e046754321a8e5c07441c63dc038971ef2963cc2
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-06-13 17:02:07 +0530

    updated variables
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -75,3 +75,18 @@ variable "kubeseal_namespace" {
 
 variable "cp_entitlement_key" {
 }
+
+variable "resource_group_name" {
+  type        = string
+  description = "Existing resource group where the IKS cluster will be provisioned."
+}
+
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "The api key for IBM Cloud access"
+}
+
+variable "region" {
+  type        = string
+  description = "Region for VLANs defined in private_vlan_number and public_vlan_number."
+}
